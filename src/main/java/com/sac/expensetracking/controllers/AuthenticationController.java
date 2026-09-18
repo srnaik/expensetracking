19c216b9ac981c80309c97b6f3a3b2f8f4a8800c
srnaik@hotmail.com <srnaik@hotmail.com>
2026-09-17 22:42:08 -0700

    Signup API Implementation

--- src/main/java/com/sac/expensetracking/controllers/AuthenticationController.java
+++ src/main/java/com/sac/expensetracking/controllers/AuthenticationController.java
@@ -1,6 +1,10 @@
 package com.sac.expensetracking.controllers;
 
 
+import com.sac.expensetracking.models.User;
+import com.sac.expensetracking.payload.request.RegisterUserPayload;
+import com.sac.expensetracking.payload.response.MessageResponse;
+import com.sac.expensetracking.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import com.sac.expensetracking.payload.request.LoginRequestPayload;
 import com.sac.expensetracking.payload.response.JwtResponse;
@@ -23,7 +28,13 @@ public class AuthenticationController {
     AuthenticationManager authenticationManager;
 
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
     JwtUtils jwtUtils;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequestPayload loginRequestPayload){
@@ -38,5 +49,21 @@ public class AuthenticationController {
 
         return ResponseEntity.ok(new JwtResponse(jwt,userDetails.getId(), userDetails.getUsername(),userDetails
                 .getFirstName()));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserPayload
+                                           registerUserPayload){
+        if (userRepository.existsByEmail(registerUserPayload.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email ID is already taken!"));
+        }
+
+        User user = new User(registerUserPayload.getEmail()
+        , passwordEncoder.encode(registerUserPayload.getPassword()),registerUserPayload.getFirstName()
+        ,registerUserPayload.getLastName());
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User Registered successfully!"));
     }
 }
--- src/main/java/com/sac/expensetracking/payload/response/JwtResponse.java
+++ src/main/java/com/sac/expensetracking/payload/response/JwtResponse.java
@@ -6,14 +6,14 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private UUID id;
-    private String username;
     private String email;
+    private String firstName;
 
-    public JwtResponse(String accessToken, UUID id, String username, String email) {
+    public JwtResponse(String accessToken, UUID id, String email, String firstName) {
         this.token = accessToken;
         this.id = id;
-        this.username = username;
         this.email = email;
+        this.firstName = firstName;
     }
 
     public String getAccessToken() {
@@ -40,19 +40,19 @@ public class JwtResponse {
         this.id = id;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 }
